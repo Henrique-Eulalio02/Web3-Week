42afe9a82ea841c5f3edd70b9348467d8fab6c8f
axolote123 <henrique.eulalio3@gmail.com>
2024-02-03 21:13:07 -0300

    feat: adding /vote route and features

--- src/app/vote/page.js
+++ src/app/vote/page.js
@@ -1,20 +1,71 @@
 "use client"
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from "next/head";
-import { doLogin } from "@/services/Web3Service";
+import { getCurrentVoting, addVote } from "@/services/Web3Service";
+import { useRouter } from 'next/navigation';
 
 export default function Vote() {
-  const [message, setMessage] = useState();
+  const { push } = useRouter();
 
-  function btnLoginClick() {
-    doLogin()
-      .then(account => setMessage(account))
+  const DEFAULT_OPTION = { name: "Loading...", image: "https://i0.statig.com.br/bancodeimagens/ao/z5/hh/aoz5hh00gkrgjje1rq2laod9k.jpg" };
+
+  const [message, setMessage] = useState("");
+  const [voting, setVoting] = useState({ maxDate: Date.now() });
+  const [option1, setOption1] = useState(DEFAULT_OPTION);
+  const [option2, setOption2] = useState(DEFAULT_OPTION);
+  const [showVotes, setShowVotes] = useState(0);
+
+
+  useEffect(() => {
+    if(!localStorage.getItem("wallet")) return push("/"); 
+
+    getCurrentVoting()
+      .then(voting => {
+        console.log(voting);
+        setVoting(voting);
+        setOption1(getOption(voting.option1));
+        setOption2(getOption(voting.option2));
+      })
       .catch(err => {
         console.error(err);
         setMessage(err.message);
       })
+  }, []);
+
+  function getOption(option) {
+    switch(option) {
+      case "raul": return { name: "Raul", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZCldKgmO2Hs0UGk6nRClAjATKoF9x2liYYA&usqp=CAU" }
+      case "pablo": return { name: "Pablo", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ3IlOhNUR958RfPptJypd8a01aj2oEGjFWg&usqp=CAU" }
+      default: return DEFAULT_OPTION;
+    }
   }
+
+  function btnVote2Click() {
+    setMessage("Conectando na carteira...aguarde...");
+    addVote(2)
+        .then(() => {
+            setShowVotes(2);
+            setMessage("Resultados parciais sujeitos a alteração minuto a minuto.");
+        })
+        .catch(err => {
+            console.error(err);
+            setMessage(err.message);
+        })
+}
+
+function btnVote1Click() {
+    setMessage("Conectando na carteira...aguarde...");
+    addVote(1)
+        .then(() => {
+            setShowVotes(1);
+            setMessage("Resultados parciais sujeitos a alteração minuto a minuto.");
+        })
+        .catch(err => {
+            console.error(err);
+            setMessage(err.message);
+        })
+}
 
   return (
     <>
@@ -27,6 +78,41 @@ export default function Vote() {
         <div className="row align-items-center">
           <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Webbb3</h1>
           <p className="lead">Votação on-chain do BBB.</p>
+          {
+            voting.maxDate > (Date.now() / 1000)
+            ? <p className="lead mb-3">Você tem até {new Date(Number(voting.maxDate) * 1000).toString()} para deixar seu voto em um dos participantes abaixo para que ele saia do programa</p>
+            : <p className="lead mb-3">Votação encerrada! Confira abaixo os resultados.</p>
+          }
+        </div>
+        <div className="row flex-lg-row-reverse align-items-center g-1 py-5">
+          <div className="col-1"></div>
+          <div className="col-5">
+            <h3 className="my-2 d-block mx-auto" style={{ width: 250 }}>{voting.option2}</h3>
+            <img src={option2.image} className="d-block mx-auto rounded" width="250" height="250" />
+            {
+              showVotes > 0 || voting.maxDate < (Date.now() / 1000)
+              ? <button className="btn btn-secondary p-3 my-2 d-block mx-auto" style={{ width: 250 }} disabled={true}>
+                {showVotes === 2 ? Number(voting.votes2) + 1 : Number(voting.votes2)} votos
+              </button>
+              : <button className="btn btn-primary p-3 my-2 d-block mx-auto" style={{ width: 250 }} onClick={btnVote2Click}>
+                Quero que saia esse
+              </button>
+            }
+          </div>
+          <div className="col-5">
+            <h3 className="my-2 d-block mx-auto" style={{ width: 250 }}>{voting.option1}</h3>
+            <img src={option1.image} className="d-block mx-auto rounded" width="250" height="250" />
+            {
+              showVotes > 0 || voting.maxDate < (Date.now() / 1000)
+              ? <button className="btn btn-secondary p-3 my-2 d-block mx-auto" style={{ width: 250 }} disabled={true}>
+                {showVotes === 1 ? Number(voting.votes1) + 1 : Number(voting.votes1)} votos
+              </button>
+              : <button className="btn btn-primary p-3 my-2 d-block mx-auto" style={{ width: 250 }} onClick={btnVote1Click}>
+                Quero que saia esse
+              </button>
+            }
+          </div>
+          <div className="col-1"></div>
         </div>
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Webbb3, Inc</p>
--- src/services/Web3Service.js
+++ src/services/Web3Service.js
@@ -1,4 +1,7 @@
 import Web3 from "web3";
+import ABI from './ABI.json';
+
+const CONTRACT_ADDRESS = "0xbc281031B957556BabC48EbDCCBFBbC46e573F56";
 
 export async function doLogin() {
   if (!window.ethereum) throw new Error("No MetaMask found.");
@@ -9,4 +12,23 @@ export async function doLogin() {
 
   localStorage.setItem("wallet", accounts[0]);
   return accounts[0];
+}
+
+function getContract() {
+  const wallet = localStorage.getItem("wallet");
+  if (!wallet) throw new Error("Unauthorized");
+
+  const web3 = new Web3(window.ethereum);
+  return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from: wallet });
+}
+
+export async function getCurrentVoting() {
+  const contract = getContract();
+
+  return contract.methods.getCurrentVoting().call();
+}
+
+export async function addVote(choice) {
+  const contract = getContract();
+  return contract.methods.addVote(choice).send();
 }
